48a85b6cb49dd539e645b55ca9d7161a6223d2d4
Nathaniel Zeeman <nathanielzeeman@gmail.com>
2018-02-08 14:07:01 -0800

    Now displaying every type of item, replaced some images with more suitable ones

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,30 +1,8 @@
 <!-- The main view of the diablo inventory will be displayed in this page -->
 <h1>This is our Home Page</h1>
 <div id="body">
-    <!--
-	<p>What is the homepage?</p>
-	<p>It should present a selector (drop-down?) to let the user chose from the available saved equipment sets.</p>
-	<p>Once one is selected, the app should then present an image of a player equipped with the accessories from that set.</p>
-	<p>The selector could be a stock HTML form, with the dropdown and a submit button linking to a sub-controller method</p>
-	<p>which built the equipped view; or it could have some Javascript attached, to pull down the appropriate pieces through AJAX calls.</p>
-	<br>
-	<p>Click on this link for further details <a href="http://comp4711.jlparry.com/display/assignment/assignment1features">Reference</a> </p>
-    -->
-    
-    <canvas id="inventoryCanvas" width="???" height="???">Your browser does not support the HTML5 canvas tag.</canvas>
-    
-    <!--
-    <select>
-        {sets}
-        <option value="{setName}" onclick="getSet({setId})">{setName}</option>
-        {/sets}
-    </select>
-    -->
-    
+    <canvas id="inventoryCanvas">Your browser does not support the HTML5 canvas tag.</canvas>
     <script>
-        var canvas = document.getElementById("inventoryCanvas");
-        var ctx = canvas.getContext("2d");
-        
         var images = [];
         
         var bg = new Image();
@@ -39,23 +17,20 @@
         chestplate.src = "assets/images/body2.png";
         images.push(chestplate);
         
-        /*
         var shoes = new Image();
-        shoes.src = "{shoesPath}";
+        shoes.src = "assets/images/boots1.png";
         images.push(shoes);
         
         var weapon = new Image();
-        weapon.src = "{weaponPath}";
+        weapon.src = "assets/images/short_sword.png";
         images.push(weapon);
-        */
         
         var loadedImages = 0;
         for (var i = 0; i < images.length; ++i)
         {
             images[i].onload = function()
             {
-                loadedImages++;
-                if (loadedImages == images.length)
+                if (++loadedImages == images.length)
                 {
                     drawImages();
                 }
@@ -64,6 +39,9 @@
         
         function drawImages()
         {
+            var canvas = document.getElementById("inventoryCanvas");
+            var ctx = canvas.getContext("2d");
+            
             inventoryCanvas.width = bg.naturalWidth;
             inventoryCanvas.height = bg.naturalHeight;
             
@@ -71,20 +49,27 @@
             
             var helmScaleX = 95 / helmet.naturalHeight * helmet.naturalWidth;
             var helmScaleY = 95;
-            var helmStartX = 200 + helmScaleX / 2;
+            var helmStartX = 200 + 95 / 2 - helmScaleX / 2;
             var helmStartY = 32;
             ctx.drawImage(helmet, helmStartX, helmStartY, helmScaleX, helmScaleY);
             
-            var chestScaleX = 170 / chestplate.naturalHeight * chestplate.naturalWidth;;
+            var chestScaleX = 170 / chestplate.naturalHeight * chestplate.naturalWidth;
             var chestScaleY = 170;
-            var chestStartX = 187 + chestScaleX / 4;
+            var chestStartX = 187 + 120 / 2 - chestScaleX / 2;
             var chestStartY = 136;
             ctx.drawImage(chestplate, chestStartX, chestStartY, chestScaleX, chestScaleY);
-        }
-        
-        function getSet()
-        {
-            // either call controller method OR make ajax call to get img paths
+            
+            var shoeScaleX = 128 / shoes.naturalHeight * shoes.naturalWidth;
+            var shoeScaleY = 128;
+            var shoeStartX = 200 + 96 / 2 - shoeScaleX / 2;
+            var shoeStartY = 510;
+            ctx.drawImage(shoes, shoeStartX, shoeStartY, shoeScaleX, shoeScaleY);
+            
+            var weaponScaleX = 190 / weapon.naturalHeight * weapon.naturalWidth;
+            var weaponScaleY = 190;
+            var weaponStartX = 38 + 95 / 2 - weaponScaleX / 2;
+            var weaponStartY = 448;
+            ctx.drawImage(weapon, weaponStartX, weaponStartY, weaponScaleX, weaponScaleY);
         }
     </script>
 </div>
