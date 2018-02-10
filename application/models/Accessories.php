ce97f6772b3cb1039b80da6e2df023d4781e643f
rostgoat <rdavidoff@hotmail.ca>
2018-02-09 19:24:59 -0800

    reconfigured catalog view page

--- application/models/Accessories.php
+++ application/models/Accessories.php
@@ -2,6 +2,16 @@
 
 class Accessories extends CSV_Model
 {
+
+	public $id;
+	public $name;
+	public $category;
+	public $materials;
+	public $weight;
+	public $imagepath;
+	public $damage;
+	public $protection;
+
 	public function __construct()
 	{
 		parent::__construct("..\data\Accessory.csv", 'id');
