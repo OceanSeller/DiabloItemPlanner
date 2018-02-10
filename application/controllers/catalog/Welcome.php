70d80e19e2bda43d858897773d8348733ccfa3b8
JohnKim <Applenourishment@gmail.com>
2018-02-09 16:09:57 -0800

    Quickly ensured that the catalog page will show

--- application/controllers/catalog/Welcome.php
+++ application/controllers/catalog/Welcome.php
@@ -8,12 +8,12 @@ class Welcome extends Application
 	 */
 	public function index()
 	{
-        $accessories = $this->accessories->all();
-
-        $categories = $this->categories->all();
-
-        $this->data['accessories'] = $accessories;
-        $this->data['categories'] = $categories;
+        // $accessories = $this->accessories->all();
+        //
+        // $categories = $this->categories->all();
+        //
+        // $this->data['accessories'] = $accessories;
+        // $this->data['categories'] = $categories;
         $this->data['pagebody'] = 'catalog';
 		//$this->data['pagetitle'] = 'OceanSeller'; /* May need to change later. */
 
