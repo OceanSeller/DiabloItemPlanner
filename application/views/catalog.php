9732883aee7fa30d7ca843d48e84556782b02098
Davin Deol <davindeol@gmail.com>
2018-04-07 17:59:31 -0700

    Restored Catalog to Former Code

--- application/views/catalog.php
+++ application/views/catalog.php
@@ -7,22 +7,18 @@
           <th>Protection</th>
         </tr>
 <tbody>
-    <div id="all-items" class="items">
-        {categories}
-            <tr class="item_row">
-                <th colspan="5" class="categoryTableHeading">{name}</th>
-            </tr>
-            {accessories}
-                <tr class="item_row">
-                    <td><img src="{imagepath}"/></td>
-                    <td><h4>{name}</h4></td>
-                    <td><p>{weight}</p></td>
-                    <td><p>{damage}</p></td>
-                    <td><p>{protection}</p></td>
-                </tr>
-            {/accessories}
-        {/categories}
-    </div>
+  <div id="all-items" class="items">
+    {accessories}
+        <tr class="item_row">
+          <td><img class="draggable drag-drop " src="{imagepath}"/></td>
+          <td><h4>{name}</h4></td>
+          <td><p>{weight}</p></td>
+          <td><p>{damage}</p></td>
+          <td><p>{protection}</p></td>
+        </tr>
+      </div>
+    {/accessories}
+  </div>
 </tbody>
 </table>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
